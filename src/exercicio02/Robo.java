55be1c8c70949bc92fc2924399e831b87b02a9c2
RafaelS <rafaelalmeida2301@gmail.com>
2025-08-27 20:03:52 -0300

    Subindo nova classe

--- src/exercicio02/Robo.java
+++ src/exercicio02/Robo.java
@@ -21,7 +21,28 @@ public abstract class Robo {
         this.x = x;
         this.y = y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        if(x >= 0 && x <= tela.getMaxX()){
+            this.x = x;
+        }
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        if(y >= 0 && y <= tela.getMaxY()){
+            this.y = y;
+        }
+    }
     
+    @Override
     public String toString(){
         return "[" + x + "," + y + "]";
     }
---
+++ src/exercicio02/RoboSimples.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio02;
+
+/**
+ *
+ * @author profslvo
+ */
+public class RoboSimples extends Robo {
+    
+    public RoboSimples(Tela tela){
+        super(tela);
+    }
+    
+    public RoboSimples(Tela tela, int x, int y){
+        super(tela, x, y);
+    }
+
+    @Override
+    public void moverNorte(int distancia) {
+    
+    }
+
+    @Override
+    public void moverSul(int distancia) {
+    
+    }
+
+    @Override
+    public void moverLeste(int distancia) {
+    
+    }
+
+    @Override
+    public void moverOeste(int distancia) {
+    
+    }
+    
+}
